9ea43a4fd1f5801c9c1978b0d2d14ecb568b9884
wzw <wzw@126.com>
2020-08-31 13:12:50 +0800

    Test888加了Hello6

--- src/main/java/com/atguigu/controller/Test888.java
+++ src/main/java/com/atguigu/controller/Test888.java
@@ -2,6 +2,8 @@ package com.atguigu.controller;
 
 public class Test888 {
     public static void main(String[] args) {
+
         System.out.println("hello");
+        System.out.println("hello6");
     }
 }
