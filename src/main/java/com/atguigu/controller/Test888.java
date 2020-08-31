360b9e9393aa810106d06312a51111cc54912686
wzw <wzw@126.com>
2020-08-31 12:53:44 +0800

    修改了Test888

--- src/main/java/com/atguigu/controller/Test888.java
+++ src/main/java/com/atguigu/controller/Test888.java
@@ -3,5 +3,9 @@ package com.atguigu.controller;
 public class Test888 {
     public static void main(String[] args) {
         System.out.println("hello");
+		System.out.println("hello2");
+		System.out.println("hello3");
+		System.out.println("hello4");
+		System.out.println("hello5");
     }
 }
